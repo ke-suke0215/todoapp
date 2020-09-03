6d18c992ad06c6e1fcc76cccbbf3b96948406296
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2020-09-03 09:49:21 +0900

    削除ボタンの修正

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react'
 import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+    extendedIcon: {
+        marginRight: theme.spacing(1),
+    },
+}));
+
 
 const Item = ({ content, id, deleteTodo, index }) => {
+    const classes = useStyles();
     const [isDone, setIsDone] = useState(false)
 
     const handleDelete = () => {
@@ -19,7 +35,9 @@ const Item = ({ content, id, deleteTodo, index }) => {
             <span style={
                 { textDecoration: isDone ? 'line-through' : 'none' }
             }>{content}</span>
-            <button onClick={handleDelete}>削除</button>
+            <IconButton onClick={handleDelete} aria-label="delete" className={classes.margin}>
+                <DeleteIcon />
+            </IconButton>
         </li>
     )
 }
